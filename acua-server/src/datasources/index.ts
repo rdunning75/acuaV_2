99ee1a845b31aaf2eee3a1d7fb4d5d0fbc4fedfd
rdunning75 <ryandunning57@gmail.com>
2019-10-30 01:57:41 -0700

    fixed merge conflicts again

--- acua-server/src/datasources/index.ts
+++ acua-server/src/datasources/index.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 export * from './mongo.datasource';
 export * from './mysql.datasource';
 export * from './microsql.datasource';
-=======
-// export * from './mongo.datasource';
-// export * from './sql-data-source.datasource';
-// export * from './sql-data-source-data-source.datasource';
-export * from './mysql.datasource';
->>>>>>> d5a8d963b97dd8c60fdc4055c2adda3f289a7771
+
